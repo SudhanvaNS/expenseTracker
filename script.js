14fd19ef9489bf686037df8b9b0fd668c8a2a330
sudhanva <sudhanva0703@gmail.com>
2023-05-27 20:40:40 +0530

    modifies the ui a bit and completes js file

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const formEl=document.getElementById("transaction-form");
 const amountEl=document.getElementById("amount");
 const descriptionEl=document.getElementById("description");
 const typeEl=document.getElementById("type");
+var balance=0.00;
 
 // event Handling of submit button
 
@@ -15,7 +16,7 @@ formEl.addEventListener("submit" ,function(event){
   const obj=new Object(desc,amount,type);
   // append an object to an array
   transaction.push(obj);
-  displayTrasactions();
+  displayTrasactions(transaction[transaction.length-1]);
 });
 //object constructor
 function Object(desc,amount,type){
@@ -23,5 +24,28 @@ function Object(desc,amount,type){
     this.amount=amount;
     this.type=type;
 }
+function displayTrasactions(arr){
+  const transactionListEl = document.getElementById("transaction-list");
+  const transactionItemEl = document.createElement("li");
+  const amountEl = document.createElement("span");
+  const descEl = document.createElement("span");
+  const typeEl=document.createElement("span");
+  amountEl.innerText = arr.amount;
+  descEl.innerText = arr.desc;
+  transactionItemEl.appendChild(descEl);
+  transactionItemEl.appendChild(amountEl);
+  
+  // transactionItemEl.appendChild(typeEl);
 
+  transactionListEl.appendChild(transactionItemEl);
+  if (arr.type === "income") {
+    balance += parseFloat(arr.amount);
+  } else if (arr.type === "expense") {
+    balance -= parseFloat(arr.amount);
+  }
+
+  const balanceEl = document.getElementById("balance");
+  balanceEl.innerText = "Balance: ₹"+balance.toFixed(2);
+}
 //funciotn to update the transaction list and balance display on the webpage
+
