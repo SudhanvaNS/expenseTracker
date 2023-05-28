0b0fdf6a72de77dd604bd40dbc91549bd611276c
sushma1031 <sushmajram@gmail.com>
2023-05-28 14:33:51 +0530

    Modify script
    - Separate functions to display and update balance
    - Add event listener to delete button for each list item

--- script.js
+++ script.js
@@ -5,28 +5,36 @@ console.log(deleteEl[1]);
 
 const formEl=document.getElementById("transaction-form");
 
-const amountEl=document.getElementById("amount");
-const descriptionEl=document.getElementById("description");
-const typeEl=document.getElementById("type");
-var balance=0.00;
+let balance=0.00;
 
 // event Handling of submit button
 
 formEl.addEventListener("submit", function(event){
   event.preventDefault();
+  const amountEl = document.getElementById("amount");
+  const descriptionEl = document.getElementById("description");
+  const typeEl = document.getElementById("type");
   const desc = descriptionEl.value;
   const amount = amountEl.value;
   const type = typeEl.value;
   const trans = { desc, amount, type };
   // append an object to an array
   transaction.push(trans);
-  console.log(transaction);
-  displayTrasactions(trans);
+  displayTrasaction(trans);
+  updateBalance(trans);
 });
-//funciotn to update the transaction list and balance display on the webpage
-function displayTrasactions(trans){
-  const transactionListEl = document.getElementById("transaction-list");
+
+const transactionItems = document.querySelectorAll(".trans-item");
+for (item of transactionItems) {
+  console.log(item.lastElementChild);
+    item.lastElementChild.addEventListener("click", () => {
+    console.log(this);
+  })
+}
+
+function displayTrasaction(trans) {
   const transactionItemEl = document.createElement("li");
+  transactionItemEl.className = "trans-item";
   const amountEl = document.createElement("span");
   amountEl.className = "trans-amt";
   const descEl = document.createElement("span");
@@ -35,15 +43,34 @@ function displayTrasactions(trans){
   btnEl.className = "btn";
   amountEl.innerText = "₹ " + trans.amount;
   descEl.innerText = trans.desc;
-  btnEl.innerText = "-";
+  typeEl.innerText = trans.type;
+
+  const editBtn = document.createElement("button");
+  editBtn.className = "edit-trans";
+  editBtn.innerHTML = '<i class="fa-sharp fa-solid fa-pen" style="color: #323232;"></i>';
+
+  const delBtn = document.createElement("button");
+  delBtn.className = "delete-trans";
+  delBtn.innerHTML =
+    '<i class="fa-solid fa-trash" style="color: #323232;"></i>';
+  
   transactionItemEl.appendChild(descEl);
   transactionItemEl.appendChild(amountEl);
-  transactionItemEl.appendChild(btnEl);
-  
-  
-  // transactionItemEl.appendChild(typeEl);
-
+  transactionItemEl.appendChild(editBtn);
+  transactionItemEl.appendChild(delBtn);
+ 
+  const transactionListEl = document.getElementById("transaction-list");
   transactionListEl.appendChild(transactionItemEl);
+}
+
+
+
+function deleteTransaction() {
+  
+}
+
+function updateBalance(trans) {
+  
   if (trans.type === "Income") {
     balance += parseFloat(trans.amount);
   } else if (trans.type === "Expense") {
@@ -58,6 +85,7 @@ function displayTrasactions(trans){
     balanceEl.classList.remove("negative-balance");
 
 }
+//function to update the transaction list and balance display on the webpage
 
 //function to delete a particular transaction
 
